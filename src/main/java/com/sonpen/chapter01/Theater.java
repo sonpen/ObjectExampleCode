5d79cfb58cb1bd742199c7eb51da6d011a6fe297
sonpen <sonpen76@naver.com>
2020-01-19 14:49:12 +0900

    [chapter01] 문제점이 있는 코드. 절차적 프로그래밍

---
+++ src/main/java/com/sonpen/chapter01/Theater.java
@@ -0,0 +1,24 @@
+package com.sonpen.chapter01;
+
+/**
+ * Created by sonpen on 20. 1. 19.
+ */
+public class Theater {
+    private TicketSeller ticketSeller;
+
+    public Theater(TicketSeller ticketSeller) {
+        this.ticketSeller = ticketSeller;
+    }
+
+    public void enter(Audience audience) {
+        if(audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
+}
